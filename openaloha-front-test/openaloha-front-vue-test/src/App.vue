bc5471da40bb0f83b3ddac008f75c4854bee5d1e
openaloah <wpplane@homeserver.local>
2025-09-01 00:23:05 +0800

    test

--- openaloha-front-test/openaloha-front-vue-test/src/App.vue
+++ openaloha-front-test/openaloha-front-vue-test/src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="HelloOpenAloha14" />
+      <HelloWorld msg="HelloOpenAloha15" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
