fc91a78a77d1e465e71f55b2623172f73f08af4f
linzizhong <linzizhong@gimc.cn>
2025-09-01 19:33:49 +0800

    test

--- openaloha-java-test/openaloha-java-springboot-test/src/main/java/com/example/demo/controller/TestController.java
+++ openaloha-java-test/openaloha-java-springboot-test/src/main/java/com/example/demo/controller/TestController.java
@@ -8,6 +8,6 @@ public class TestController {
 
     @GetMapping("/test")
     public String test() {
-        return "Hello, OpenAloha SpringBoot2!";
+        return "Hello, OpenAloha SpringBoot3!";
     }
 }
